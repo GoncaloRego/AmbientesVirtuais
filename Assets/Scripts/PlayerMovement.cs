bc27a39178da6ba054015c135781f7a5df9fc46f
GoncaloRego <goncalo-fcp@hotmail.com>
2016-12-22 15:37:15 +0000

    ...

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpSpeed;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+    void MovePlayer()
+    {
+        float moveHorizontal, moveVertical, jump;
+        Vector3 movement;
+
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        jump = Input.GetAxis("Jump");
+
+        movement = new Vector3(moveHorizontal, jump * jumpSpeed, moveVertical);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
